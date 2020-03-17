acb25b48eedaa39ee465b7d20f735ef4a284fe9d
Steppanov Mikhail <follout13@gmail.com>
2020-03-17 17:02:48 +0300

    Hamburger

--- src/js/main.js
+++ src/js/main.js
@@ -36,21 +36,9 @@ $(document).ready(function () {
   var duration = 400;
   $(".btn").click(function () {
     $(".nav-bar").toggle(effect, options, duration);
+    $('.btn').toggleClass("active");
   });
 
-  $(".btn").click(function () {
-    $(this).toggleClass("active");
-  });
-  $(document).mouseup(function (e) { 
-    var block = $(".nav-bar");
-    if (!block.is(e.target) // проверка условия если клик был не по нашему блоку
-      &&
-      block.has(e.target).length === 0) { // проверка условия если клик не по его дочерним элементам
-      
-      $(".nav-bar").hide(effect, options, duration);
-      $('.btn').removeClass("active"); // если условия выполняются - скрываем наш элемент
-    }
-  });
   $(document).scroll(function () {
     if ($(this).scrollTop() > 300) {
       $(".nav-bar").hide(effect, options, duration);

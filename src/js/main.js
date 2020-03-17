632b71699b64f7689dcd3e7be258fd22c562e59d
Steppanov Mikhail <follout13@gmail.com>
2020-03-17 16:04:43 +0300

    Анимация

--- src/js/main.js
+++ src/js/main.js
@@ -10,18 +10,51 @@ $(document).ready(function() {
   aboutInit();
   coursesinit();
   reviewsInit();
-  headerinit();
 });
 
-$(".btn").click(function () {
+
+// Анимации
+
+
+
+$(document).ready(function () {
+  $("a.scrollto").click(function () {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top - 50;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
+});
+
+$(document).ready(function () {
   var options = {
     direction: "right"
   };
   var effect = 'slide';
   var duration = 400;
-  $(".nav-bar").toggle(effect, options, duration);
-});
+  $(".btn").click(function () {
+    $(".nav-bar").toggle(effect, options, duration);
+  });
 
-$(".btn").click(function () {
-  $(this).toggleClass("active");
+  $(".btn").click(function () {
+    $(this).toggleClass("active");
+  });
+  $(document).mouseup(function (e) { 
+    var block = $(".nav-bar");
+    if (!block.is(e.target) // проверка условия если клик был не по нашему блоку
+      &&
+      block.has(e.target).length === 0) { // проверка условия если клик не по его дочерним элементам
+      
+      $(".nav-bar").hide(effect, options, duration);
+      $('.btn').removeClass("active"); // если условия выполняются - скрываем наш элемент
+    }
+  });
+  $(document).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".nav-bar").hide(effect, options, duration);
+      $('.btn').removeClass("active");
+    }
+  });
 });

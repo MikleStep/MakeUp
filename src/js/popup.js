84d5b1e8e12717c8604a23e532d9de9c0af6de8f
Steppanov Mikhail <follout13@gmail.com>
2020-03-19 21:12:53 +0300

    Модальные окна

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,3 +1,30 @@
 export const init = () => {
-  
-};
+  $('.header__button').click(function () {
+    $('.calendar__container').addClass('calendar__container--shown');
+    $('body').addClass('modal-open');
+  });
+  $('.header__phone--button').click(function () {
+    $('.calendar__container').addClass('calendar__container--shown');
+    $('body').addClass('modal-open');
+  });
+  $('.cta__btn').click(function () {
+    $('.calendar__container').addClass('calendar__container--shown');
+    $('body').addClass('modal-open');
+  });
+  $('.modal__button--course').click(function () {
+    $('.calendar__container').addClass('calendar__container--shown');
+    $('body').addClass('modal-open');
+  });
+  $('.calendar__window--close').click(function () {
+    $('.calendar__container').removeClass('calendar__container--shown');
+    $('body').removeClass('modal-open');
+  });
+  $(document).mouseup(function (e) {
+    var container = $(".calendar__container");
+    if (container.has(e.target).length === 0 && container.hasClass('calendar__container--shown')) {
+      container.removeClass('calendar__container--shown');
+      $('body').removeClass('modal-open');
+    }
+  });
+
+}

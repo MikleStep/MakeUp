6f75b85b0c1383ecc21149e5c4e6bcd8f78824ea
Steppanov Mikhail <follout13@gmail.com>
2020-03-24 21:14:38 +0300

    Формы обратной связи

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,24 +1,4 @@
 export const init = () => {
-  $('.header__button').click(function () {
-    $('.calendar__container').addClass('calendar__container--shown');
-    $('body').addClass('modal-open');
-  });
-  $('.header__phone--button').click(function () {
-    $('.calendar__container').addClass('calendar__container--shown');
-    $('body').addClass('modal-open');
-  });
-  $('.cta__btn').click(function () {
-    $('.calendar__container').addClass('calendar__container--shown');
-    $('body').addClass('modal-open');
-  });
-  $('.modal__button--course').click(function () {
-    $('.calendar__container').addClass('calendar__container--shown');
-    $('body').addClass('modal-open');
-  });
-  $('.calendar__window--close').click(function () {
-    $('.calendar__container').removeClass('calendar__container--shown');
-    $('body').removeClass('modal-open');
-  });
   $(document).mouseup(function (e) {
     var container = $(".calendar__container");
     if (container.has(e.target).length === 0 && container.hasClass('calendar__container--shown')) {
@@ -26,5 +6,22 @@ export const init = () => {
       $('body').removeClass('modal-open');
     }
   });
+  $(function () {
+    var two_modal = function (id_modal_1, id_modal_2) {
+      var show_modal_2 = false;
+      $('a[href="' + id_modal_2 + '"]').click(function (e) {
+        e.preventDefault();
+        show_modal_2 = true;
+        $(id_modal_1).modal('hide');
+      });
+      $(id_modal_1).on('hidden.bs.modal', function (e) {
+        if (show_modal_2) {
+          show_modal_2 = false;
+          $(id_modal_2).modal('show');
+        }
+      })
 
+    }('#modal-yourself', '#modal-course');
+
+  });
 }
